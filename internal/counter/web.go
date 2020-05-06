385470a264eebc16a5bc5c03f4087cd7ec4b1cf7
nvm <Never M>
2020-05-06 19:00:13 +0300

    Add cookie-based user counting

--- internal/counter/web.go
+++ internal/counter/web.go
@@ -3,23 +3,49 @@ package counter
 import (
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 )
 
+const visitedCookieName = "visited"
+
 type App struct {
 	numClients int
+	mux        sync.Mutex
 }
 
 func StartApp() {
 	app := &App{}
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		app.numClients++
+		if !hasVisited(c) {
+			setVisited(c)
+			app.mux.Lock()
+			app.numClients++
+			app.mux.Unlock()
+		}
 		log.Println("Serving response!")
 		c.JSON(http.StatusOK, gin.H{
 			"clients": app.numClients,
 		})
 	})
 	r.Run(":8080")
+}
+
+// true if user has visited cookie
+func setVisited(c *gin.Context) {
+	// todo: secure cookie
+	cookie := &http.Cookie{Name: visitedCookieName, Value: "1"}
+	http.SetCookie(c.Writer, cookie)
+}
+
+// true if user has visited cookie
+func hasVisited(c *gin.Context) bool {
+	// todo: validate cookie
+	_, err := c.Request.Cookie(visitedCookieName)
+	if err != nil {
+		return false
+	}
+	return true
 }
