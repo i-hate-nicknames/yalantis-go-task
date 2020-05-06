0270b260930ef6105600e717bd02468ee65c072a
nvm <Never M>
2020-05-06 18:44:33 +0300

    Increment number of cliens on each request

--- internal/counter/web.go
+++ internal/counter/web.go
@@ -7,11 +7,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type App struct {
+	numClients int
+}
+
 func StartApp() {
+	app := &App{}
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
+		app.numClients++
 		log.Println("Serving response!")
-		c.JSON(http.StatusNoContent, gin.H{})
+		c.JSON(http.StatusOK, gin.H{
+			"clients": app.numClients,
+		})
 	})
 	r.Run(":8080")
 }
